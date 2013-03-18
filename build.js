c7b8e7c22ae5339b25ba3a454b514d0161e7cb7e
chris lindsey <56clindsey@gmail.com>
2013-03-17 22:15:45 -0700

    saving

--- build.js
+++ build.js
@@ -4,20 +4,25 @@
   paths: {
     jQuery: "../../vendor/jquery/jquery.dev.1.9.1",
     underscore: "../../vendor/underscore/underscore.dev.1.4.4",
+    text: "../../vendor/text/text.dev.2.0.3",
     Backbone: "../../vendor/backbone/backbone.dev.0.9.10",
     Bootstrap: "../../vendor/bootstrap/2.3.0/js/bootstrap",
     Alea: "../../vendor/alea/alea",
     Base: "../../vendor/base/base",
     Machine: "../../vendor/machine/machine",
-    astar: "../../vendor/astar/astar"
+    AStar: "../../vendor/astar/astar"
   },
 
   shim: {
     underscore: {
       exports: "_"
     },
+    jQuery: {
+      exports: "$"
+    },
     Backbone: ["underscore", "jQuery"],
-    Machine: ["Base"]
+    Machine: ["Base"],
+    Bootstrap: [ "jQuery" ]
   },
 
   name: "main",
